e99601e30f152537faf4df82b9fb076615f4ea18
IssaHadhoud <issaabuhadhoud@gmail.com>
2022-06-25 22:08:00 +0300

    edit price field

--- Model/Cart.cs
+++ Model/Cart.cs
@@ -2,9 +2,10 @@ namespace backend.Model
 {
     public class Cart
     {
+        private int _price;
         public int ID { get; set; }
         public string Name { get; set; }
-        public double Price { get; set; }
+        public int Price { get { return _price; } set { _price = value; } }
 
     }
 }
